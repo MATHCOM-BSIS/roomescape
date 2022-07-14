f75890b9a598783e48772336a9031b392499077a
given-woo <2210220@bsis.hs.kr>
2022-07-14 20:40:51 +0900

    hold event on w

--- src/js/w.js
+++ src/js/w.js
@@ -2,4 +2,4 @@ const check_Ans = () => {
     const input = document.getElementsByClassName("answer")[0].value;
     if (input == 21) window.location.href = `/solvedw.html`;
     else alert("Try Again");
-}
+};
